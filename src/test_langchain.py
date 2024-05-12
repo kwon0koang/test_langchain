182a9904090d615312b4b609c09d43f6b3e137a3
kwon0koang <kwon0koang@gmail.com>
2024-05-12 18:54:40 +0900

    add openai

--- src/test_langchain.py
+++
@@ -1,5 +0,0 @@
-from langchain_community.chat_models import ChatOllama
-
-llm = ChatOllama(model="llama3:latest")
-baseMessage = llm.invoke("What is Yeouido?")
-print(f'baseMessage : {baseMessage}')
