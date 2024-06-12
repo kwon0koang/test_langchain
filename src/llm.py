f51336c827f5252ac95670eba497eac9e1e1aadb
kwon0koang <kwon0koang@gmail.com>
2024-06-13 08:32:56 +0900

    [kyk] rag chain temperature 수정

--- src/llm.py
+++ src/llm.py
@@ -1,3 +1,3 @@
 from langchain_community.chat_models import ChatOllama
 
-llm = ChatOllama(model="EEVE-Korean-Instruct-10.8B-v1.0:latest")
+llm = ChatOllama(model="EEVE-Korean-Instruct-10.8B-v1.0:latest", temperature=0)
