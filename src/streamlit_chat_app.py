6230ea967bf8aa5bd783e1001dbf48e348aec93c
kwon0koang <kwon0koang@gmail.com>
2024-06-16 22:08:28 +0900

    [kyk] streamlit chat 수정

--- src/streamlit_chat_app.py
+++ src/streamlit_chat_app.py
@@ -5,7 +5,8 @@ from langchain_community.chat_models import ChatOllama
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 import streamlit as st
-
+from streamlit.runtime.state import SessionStateProxy
+import time
 
 class StreamHandler(BaseCallbackHandler):
     def __init__(self, container, initial_text=""):
@@ -15,6 +16,8 @@ class StreamHandler(BaseCallbackHandler):
     def on_llm_new_token(self, token: str, **kwargs) -> None:
         self.text += token
         self.container.markdown(self.text)
+        
+st.title("My name is 권봇 🤖")
 
 llm = ChatOllama(model="EEVE-Korean-Instruct-10.8B-v1.0:latest")
 
@@ -25,11 +28,8 @@ prompt = ChatPromptTemplate.from_messages([
 
 chain = prompt | llm
         
-# with st.sidebar:
-#     openai_api_key = st.text_input("OpenAI API Key", type="password")
-
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [AIMessage(role="ai", content="무엇을 도와드릴까요?")]
+    st.session_state.messages = [AIMessage(role="ai", content="무엇을 도와드릴까요?")]
 
 for msg in st.session_state.messages:
     print(f"for msg in st.session_state.messages / msg.content: {msg.content}")
@@ -39,17 +39,9 @@ if prompt := st.chat_input():
     st.session_state.messages.append(HumanMessage(role="human", content=prompt))
     st.chat_message("human").write(prompt)
 
-    # if not openai_api_key:
-    #     st.info("Please add your OpenAI API key to continue.")
-    #     st.stop()
-
-    # with st.chat_message("assistant"):
-    #     stream_handler = StreamHandler(st.empty())
-    #     llm = ChatOpenAI(openai_api_key=openai_api_key, streaming=True, callbacks=[stream_handler])
-    #     response = llm.invoke(st.session_state.messages)
-    #     st.session_state.messages.append(ChatMessage(role="assistant", content=response.content))
     with st.chat_message("ai"):
         print(f"messages: {st.session_state.messages}")
         stream_handler = StreamHandler(st.empty())
         response = chain.invoke({"messages": st.session_state.messages}, {"callbacks": [stream_handler]})
+        time.sleep(0.1)
         st.session_state.messages.append(AIMessage(role="ai", content=response.content))
